7dc83c2de44357243750990458e7acc44df5e698
Eng Soon Cheah <cheahengsoon@rocketmail.com>
2017-05-12 13:53:31 +0800

    Xamarin For Azure Mobile Services and Storage
    Using Azure Mobile Services and Storage for Xamarin

---
+++ HOL 1/Complete/GPSImageTag/GPSImageTag.Shared.Api/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPSImageTag
+{
+    public static class Configuration
+    {
+        /// <summary>
+        /// Azure Storage Connection String. UseDevelopmentStorage=true points to the storage emulator.
+        /// </summary>
+        // public const string StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=<Azure Storage account name goes here>;AccountKey=<Azure Storage account key goes here>";
+        public const string StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=petfrenzblob;AccountKey=zFc5dXh6f5CH13K3IIvBphEyCumAwl/8dXOJz+BLS/Sbg2AjhNQMDOBT65LYe8OfnADfFfeKjiK2azpaq3SdZw==;";
+        public const string StorageContainerName = "photos";
+    }
+}
